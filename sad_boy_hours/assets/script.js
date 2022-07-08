c52ec8bc61ead5b0c3b8bd7c2a73a9ae278147a5
carambola <celestebaltazar@yahoo.com>
2022-07-07 17:52:01 -0700

    added function tabs that represent mood playlist

--- sad_boy_hours/assets/script.js
+++ sad_boy_hours/assets/script.js
@@ -1,1 +1,15 @@
 
+function tabs(evt, tabMood) {
+    var i, tabcontent, tablinks;
+    Moods = document.getElementsByClassName("sectionMood");
+    for (i = 0; i < Moods.length; i++) {
+      Moods[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabMood).style.display = "block";
+    
+    evt.currentTarget.className += " active";
+  }
